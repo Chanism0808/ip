9243400d82a6233938491e8529363ecf04a0516e
Chanism0808 <chanjustin999@gmail.com>
2025-08-21 10:42:43 +0800

    Echo function: Echos user inputs and exits when the user types "bye".
    Reads in user input to echo back to user. Exits when user input is "bye"

--- src/main/java/Dupe.java
+++ src/main/java/Dupe.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Dupe {
     public static void main(String[] args) {
         String greetings = "____________________\n"
                 + "Hello! I'm Dupe";
         System.out.println(greetings);
         query();
-        exit();
+        while (true){
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if(!input.equals("bye")){
+                System.out.println("____________________\n"
+                                    + input
+                                    + "\n____________________");
+            }
+            else{
+                exit();
+                break;
+            }
+        }
     }
 
     public static void query() {
@@ -13,6 +27,8 @@ public class Dupe {
     }
 
     public static void exit() {
-        System.out.println("Goodbye! Hope to see you again soon!");
+        System.out.println("____________________\n"
+                        + "Goodbye! Hope to see you again soon!\n"
+                        + "____________________");
     }
 }
