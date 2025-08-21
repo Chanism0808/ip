3c9956ea04c687fe8d5c6446c1bff01e96ef7b56
Chanism0808 <chanjustin999@gmail.com>
2025-08-21 09:58:36 +0800

    Dupe Chatbot Skeleton: rename and add greeting/exit skeleton
    Rename the chatbot from "Duke" to a "Dupe".
    
    Implement a basic skeletal version of the Dupe chatbot that:
    * greets the user upon starting
    * prints a prompt asking what it can do for the user
    * exits with a exit message

---
+++ src/main/java/Dupe.java
@@ -0,0 +1,11 @@
+public class Dupe {
+    public static void main(String[] args) {
+        String intro = "________________________________________\n"
+                + "Hello! I'm Dupe\n"
+                + "What can I do for you?\n\n"
+                + "________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "________________________________________\n";
+        System.out.println(intro);
+    }
+}
