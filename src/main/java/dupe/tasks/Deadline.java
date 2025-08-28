e51c878f0e36f0cefdbc859b8ee2682f98b2df17
Chanism0808 <chanjustin999@gmail.com>
2025-08-28 17:13:59 +0800

    Modify Deadline.java

--- src/main/java/dupe/tasks/Deadline.java
+++ src/main/java/dupe/tasks/Deadline.java
@@ -1,11 +1,12 @@
 package dupe.tasks;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Deadline extends Task{
+public class Deadline extends Task {
     private LocalDateTime deadline;
 
-    public Deadline(String description, LocalDateTime deadline){
+    public Deadline(String description, LocalDateTime deadline) {
         super(description);
         this.deadline = deadline;
     }
@@ -13,14 +14,14 @@ public class Deadline extends Task{
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
-        String deadlineString = this.deadline.format(formatter); // Aug 08 2001 14:30
+        String deadlineString = this.deadline.format(formatter);  // e.g., Aug 08 2001 14:30
         return "[D]" + super.toString() + " (by: " + deadlineString + ")";
     }
 
     @Override
     public String savedListFormat() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
-        String deadlineString = this.deadline.format(formatter); // Aug 08 2001 14:30
+        String deadlineString = this.deadline.format(formatter);  // e.g., Aug 08 2001 14:30
         return "D | " + super.savedListFormat() + " | " + deadlineString;
     }
 }
