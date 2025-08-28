66ebad23a20ecd1758f19cae5d65c0f772eaa48e
Chanism0808 <chanjustin999@gmail.com>
2025-08-28 20:45:26 +0800

    Modify Storage.java with JDoc

--- src/main/java/dupe/storage/Storage.java
+++ src/main/java/dupe/storage/Storage.java
@@ -15,13 +15,28 @@ import dupe.parser.Parser;
 import dupe.ui.Ui;
 import dupe.tasks.Event;
 
+/**
+ * Handles loading tasks from and saving tasks to a file.
+ */
 public class Storage {
     private final String filePath;
 
+    /**
+     * Creates a new {@code Storage} instance with the given file path.
+     *
+     * @param filePath Path to the save file.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Loads tasks from the save file into memory.
+     * If the file does not exist, a new file is created and an empty task list is returned.
+     *
+     * @return A list of tasks loaded from the file.
+     * @throws IOException If an error occurs while creating or reading the file.
+     */
     public ArrayList<Task> load() throws IOException {
         File file = new File(filePath);
 
@@ -68,6 +83,13 @@ public class Storage {
         return taskList.getTasks();
     }
 
+    /**
+     * Saves the given tasks to the save file.
+     * If the file does not exist, a new file is created.
+     *
+     * @param tasks The list of tasks to be saved.
+     * @param ui    The {@code Ui} instance used to display error messages if saving fails.
+     */
     public void save(ArrayList<Task> tasks, Ui ui) {
         File file = new File(filePath);
 
