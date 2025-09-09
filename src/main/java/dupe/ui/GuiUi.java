01f2441ab80b6aedbb979a56b107dab06b6a2b83
Chanism0808 <chanjustin999@gmail.com>
2025-09-09 16:33:25 +0800

    Edit GuiUI.java

--- src/main/java/dupe/ui/GuiUi.java
+++ src/main/java/dupe/ui/GuiUi.java
@@ -10,6 +10,9 @@ import dupe.tasks.Task;
  */
 public class GuiUi {
 
+    /**
+     * Return a greeting message when the program starts.
+     */
     public String showGreeting() {
         return "____________________\n"
                 + "Hello! I'm Dupe\n"
@@ -17,16 +20,30 @@ public class GuiUi {
                 + "____________________";
     }
 
+    /**
+     * Return a goodbye message when the program exits.
+     */
     public String showExit() {
         return "____________________\n"
                 + "Goodbye! Hope to see you again soon!\n"
                 + "____________________";
     }
 
+    /**
+     * Return an error message to the console.
+     *
+     * @param message The error message to display.
+     */
     public String showError(String message) {
         return "Error!! " + message;
     }
 
+    /**
+     * Return a message showing that a task has been added.
+     *
+     * @param task The task that was added.
+     * @param taskCount The current number of tasks in the list.
+     */
     public String showTaskAdded(Task task, int taskCount) {
         return "____________________\n"
                 + "Got it. I've added this task:\n"
@@ -35,6 +52,11 @@ public class GuiUi {
                 + "\n____________________";
     }
 
+    /**
+     * Return the current list of tasks.
+     *
+     * @param tasks The list of tasks to display.
+     */
     public String showTaskList(ArrayList<Task> tasks) {
         if (tasks.isEmpty()) {
             return "____________________\nYou have no tasks in your list.\n____________________";
@@ -47,6 +69,11 @@ public class GuiUi {
         return sb.toString();
     }
 
+    /**
+     * Return the list of tasks that has the keyword.
+     * @param keyword The string that user wants to find.
+     * @param tasks The list of tasks to display.
+     */
     public String printFoundTasks(String keyword, ArrayList<Task> tasks) {
         StringBuilder sb = new StringBuilder("____________________\nHere are the matching tasks in your list:\n");
         int x = 1;
@@ -63,32 +90,30 @@ public class GuiUi {
         return sb.toString();
     }
 
+    /**
+     * Return a message indicating that a task has been marked as done.
+     *
+     * @param task The task that was marked done.
+     */
     public String showTaskMarked(Task task) {
         return "Nice! I've marked this task as done:\n"
                 + task
                 + "\n____________________";
     }
 
+    /**
+     * Prints a message indicating that a task has been marked as not done.
+     *
+     * @param task The task that was marked not done.
+     */
     public String showTaskUnmarked(Task task) {
         return "OK, I've marked this task as not done yet:\n"
                 + task
                 + "\n____________________";
     }
 
-    public String showTaskDeleted(Task task, int taskCount) {
-        return "____________________\n"
-                + task
-                + "\nNow you have " + taskCount + " tasks in the list."
-                + "\n____________________";
-    }
-
-    public String showListLoaded(ArrayList<Task> tasks) {
-        return "____________________\n"
-                + "Loaded " + tasks.size() + " tasks from file.";
-    }
-
     /**
-     * Displays a confirmation message when the priority of a task is set.
+     * Return a confirmation message when the priority of a task is set.
      *
      * @param task the {@link Task} whose priority was updated
      */
@@ -97,4 +122,28 @@ public class GuiUi {
                 + task
                 + "\n____________________";
     }
+
+    /**
+     * Return a message showing that a task has been deleted.
+     *
+     * @param task The task that was deleted.
+     * @param taskCount The current number of tasks remaining in the list.
+     */
+    public String showTaskDeleted(Task task, int taskCount) {
+        return "____________________\n"
+                + task
+                + "\nNow you have " + taskCount + " tasks in the list."
+                + "\n____________________";
+    }
+
+    /**
+     * Return a message indicating that a list of tasks has been loaded from a file.
+     *
+     * @param tasks The list of tasks that were loaded.
+     */
+    public String showListLoaded(ArrayList<Task> tasks) {
+        return "____________________\n"
+                + "Loaded " + tasks.size() + " tasks from file.";
+    }
+
 }
